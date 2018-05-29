01ac677a51157b5d3260c0a538ee336326bcced6
Naveen <naveennaikhnr@gmail.com>
2018-05-29 07:49:46 +0530

    error resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
