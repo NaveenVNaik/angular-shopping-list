6b0d71f54d3ac1866f5b5625c3ac1a64a4e46d8d
Naveen <naveennaikhnr@gmail.com>
2018-05-29 07:38:01 +0530

    changed folder structure

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-headercomponent',
+    templateUrl:'',
+    styleUrls:['./header.component.css']
+})
+
+export class HeaderComponent{
+
+}
+
+
+
+
+
+
